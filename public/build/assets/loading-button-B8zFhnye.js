e0afd4a672bd9930e6d945980b042d23b9503661
Isaac Hatilima <isaac.hatilima@springer-dl.de>
2025-06-25 09:40:13 +0200

    removed build

--- public/build/assets/loading-button-B8zFhnye.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./app-B5nIqswW.js";import{B as i}from"./button-pjdEQW0R.js";import{c as n}from"./createLucideIcon-DvAOrpbu.js";/**
- * @license lucide-react v0.475.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const c=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],d=n("LoaderCircle",c);function f({processing:a,text:e,tabIndex:o,fullWidth:r,destructive:s}){return t.jsxs(i,{className:r?"w-full":"",disabled:a,tabIndex:o,variant:s?"destructive":"default",children:[a&&t.jsx(d,{className:"h-4 w-4 animate-spin"}),e]})}export{f as L};
